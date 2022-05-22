3bfe2f77b08a739f224f20419121166d8e724453
Maximilian Berndt <hallo@maximilianberndt.com>
2022-05-22 19:02:07 +0200

    Add create spring, readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # tiny-spring
+
+Tiny spring implementation in 27 lines of javascript
+
+Basic Usage
+```
+// Create spring with default parameters
+const spring = createSpring(0)
+
+const tick = () =>{
+	// Update spring every tick
+	spring.update()
+
+	// Get value
+	console.log(spring.get())
+
+	window.requestAnimationFrame(tick)	
+}
+tick()
+
+// Set target value
+window.addEventListner("mousemove", (e) => {
+	spring.set(e.clientX)
+})
+```
+
+Create with custom paramters
+```
+const spring = createSpring(0, {
+	stiffness: 0.2, 
+	damping: 1, 
+	mass = 2, 
+})
+```
---
+++ index.js
@@ -0,0 +1,27 @@
+const createSpring = (
+  start,
+  { stiffness = 0.1, damping = 0.8, mass = 1 } = {}
+) => {
+  let current = start;
+  let previous = start;
+  let target = start;
+
+  const update = () => {
+    const velocity = current - previous;
+    const acceleration =
+      ((target - current) * stiffness - velocity * damping) / mass;
+
+    previous = current;
+    current += velocity + acceleration;
+  };
+
+  return {
+    update,
+    get: () => current,
+    set: (newTarget) => {
+      target = newTarget;
+    },
+  };
+};
+
+export default createSpring;
